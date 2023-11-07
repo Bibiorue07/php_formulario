1a46cf36c111e54683137bb2affeb89e9728da43
biaorue <100864209+Bibiorue07@users.noreply.github.com>
2023-11-07 15:21:28 -0300

    Add files via upload

---
+++ cad.php
@@ -0,0 +1,16 @@
+<html>
+
+<body>
+    <header>
+        <h1>Resultado do Processamento</h1>
+    </header>
+    <main>
+     <?php
+      $n= $_GET["nome"];
+      $s= $_ET["sobrenome"];
+      echo "<p> É um prazer te conhecer, <strong> $n $s</strong>! Este é o meu site;"
+     ?>
+    <p><a href="javascript:history.go(-1)">Voltar para a página ab=nterior</a></p>
+    </main>
+</body>
+</html>
